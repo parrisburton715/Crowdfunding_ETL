8fef411b02b289f58e24fcc18bee16e74bd9ad91
Nicole <162922142+nicolercambron@users.noreply.github.com>
2024-06-11 20:03:31 -0500

    Add files via upload

--- Starter_Files/Resources/crowdfunding_db_schema.sql
+++ Starter_Files/Resources/crowdfunding_db_schema.sql
@@ -38,3 +38,36 @@ CREATE TABLE crowdfunding.campaign (
     FOREIGN KEY (subcategory_id) REFERENCES crowdfunding.subcategory(subcategory_id),
     FOREIGN KEY (contact_id) REFERENCES crowdfunding.contacts(contact_id)
 );
+
+DROP TABLE IF EXISTS crowdfunding.campaign;
+DROP TABLE IF EXISTS crowdfunding.subcategory;
+
+CREATE TABLE crowdfunding.subcategory (
+    subcategory_id VARCHAR(50) PRIMARY KEY,
+    subcategory_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE crowdfunding.campaign (
+    cf_id SERIAL PRIMARY KEY,
+    contact_id INT NOT NULL,
+    company_name VARCHAR(255),
+    description TEXT,
+    goal FLOAT NOT NULL,
+    pledged FLOAT NOT NULL,
+    outcome VARCHAR(255),
+    backers_count INT,
+    country VARCHAR(255),
+    currency VARCHAR(255),
+    launch_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    category_id VARCHAR(50) NOT NULL,
+    subcategory_id VARCHAR(50) NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES crowdfunding.category(category_id),
+    FOREIGN KEY (subcategory_id) REFERENCES crowdfunding.subcategory(subcategory_id),
+    FOREIGN KEY (contact_id) REFERENCES crowdfunding.contacts(contact_id)
+);
+
+SELECT * FROM crowdfunding.category;
+SELECT * FROM crowdfunding.subcategory;
+SELECT * FROM crowdfunding.contacts;
+SELECT * FROM crowdfunding.campaign;
